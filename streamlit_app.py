d110ca165cc3cb4426757944be6079ae07b20cf1
Spurgeon109 <69966403+Spurgeon109@users.noreply.github.com>
2023-06-07 13:41:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My parents New Health Dinner")
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
